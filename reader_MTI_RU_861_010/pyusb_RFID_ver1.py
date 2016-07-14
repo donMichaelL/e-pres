72cd518876b177142c069b89486263881baa44ed
Michael <michael.loukeris@icloud.com>
2016-07-14 14:42:12 +0300

    When disconnecting the reader trying to connect again

--- reader_MTI_RU_861_010/pyusb_RFID_ver1.py
+++ reader_MTI_RU_861_010/pyusb_RFID_ver1.py
@@ -5,71 +5,119 @@ import usb.util
 import time
 import commands
 import response
-# decimal vendor and product values
-dev = usb.core.find(idVendor=0x24e9, idProduct=0x0861)
-interface = 0
-endpoint = dev[0][(0,0)][1]
-cfg = dev.get_active_configuration()
-intf = cfg[(0,0)]
 
-ep = usb.util.find_descriptor(
-    intf,
-    # match the first OUT endpoint
-    custom_match = \
-    lambda e: \
-        usb.util.endpoint_direction(e.bEndpointAddress) == \
-        usb.util.ENDPOINT_OUT)
 
-assert ep is not None
+# Connect With Reader
+def discover_reader():
+    found_reader = False
+    while not found_reader:
+        global dev
+        dev = usb.core.find(idVendor=0x24e9, idProduct=0x0861)
+        interface = 0
+        try:
+            endpoint = dev[0][(0,0)][1]
+            cfg = dev.get_active_configuration()
+            intf = cfg[(0,0)]
+            found_reader = True
+            global ep
+            ep = usb.util.find_descriptor(
+                intf,
+                # match the first OUT endpoint
+                custom_match = \
+                lambda e: \
+                    usb.util.endpoint_direction(e.bEndpointAddress) == \
+                    usb.util.ENDPOINT_OUT)
 
-#------------------- Before Inventory ---------------------#
-# cancel
-ep.write(bytearray(commands.cancel_operation))
-# get mac
-for item in commands.read_mac:
-    ep.write(bytearray(item))
+            assert ep is not None
+        except:
+            print "Not Found"
+            time.sleep(5)
+            pass
+
+
+def initialization():
+    # cancel 0x50
+    ep.write(bytearray(commands.cancel_operation))
+    # get mac 0x67(x5)
+    for item in commands.read_mac:
+        ep.write(bytearray(item))
+        response.print_dictionary(response.read_antenna(dev))
+    # get firmware 0x60
+    ep.write(bytearray(commands.get_firmware))
+    response.print_dictionary(response.read_antenna(dev))
+    # get version 0x6c
+    ep.write(bytearray(commands.get_version))
+    response.print_dictionary(response.read_antenna(dev))
+    # get update_number 0x6d
+    ep.write(bytearray(commands.get_upd_num))
+    response.print_dictionary(response.read_antenna(dev))
+    # get bootloader 0x64
+    ep.write(bytearray(commands.get_bootloader))
+    response.print_dictionary(response.read_antenna(dev))
+    # get update_number 0x07
+    ep.write(bytearray(commands.mac_registers))
     response.print_dictionary(response.read_antenna(dev))
 
 
-# get firmware
-ep.write(bytearray(commands.get_firmware))
-response.print_dictionary(response.read_antenna(dev))
+# configure antenna
+def antenna_configuration():
 
+    ep.write(bytearray(commands.set_antenna_port_state))
+    response.print_dictionary(response.read_antenna(dev))
 
-# get version
-ep.write(bytearray(commands.get_version))
-response.print_dictionary(response.read_antenna(dev))
+    ep.write(bytearray(commands.set_sense_threshold))
+    response.print_dictionary(response.read_antenna(dev))
 
+    ep.write(bytearray(commands.set_antena_config))
+    response.print_dictionary(response.read_antenna(dev))
 
-# get update_number
-ep.write(bytearray(commands.get_upd_num))
-response.print_dictionary(response.read_antenna(dev))
+def antenna_configuration_2():
+    ep.write(bytearray(commands.set_antenna_port_state_2))
+    response.print_dictionary(response.read_antenna(dev))
 
+    ep.write(bytearray(commands.set_sense_threshold_2))
+    response.print_dictionary(response.read_antenna(dev))
 
-# get update_number
-ep.write(bytearray(commands.mac_registers))
-response.print_dictionary(response.read_antenna(dev))
+    ep.write(bytearray(commands.set_antena_config_2))
+    response.print_dictionary(response.read_antenna(dev))
 
-#------------------- Before Inventory ---------------------#
+# # retrieve inventory 0x03
+# ep.write(bytearray(commands.retrieve_inventory))
+# response.print_dictionary(response.read_antenna(dev))
 
-# set mode
-ep.write(bytearray(commands.set_mode))
-response.print_dictionary(response.read_antenna(dev))
+# set mode 0x02
+def run_inventory():
+    print 'intentory'
+    ep.write(bytearray(commands.set_mode))
+    print bytearray(commands.set_mode)
+    response.print_dictionary(response.read_antenna(dev))
 
-# start inventory
-ep.write(bytearray(commands.tag_inventory))
-response.print_dictionary(response.read_antenna(dev))
+    # start inventory 0x40
+    ep.write(bytearray(commands.tag_inventory))
+    response.print_dictionary(response.read_antenna(dev))
+    response.print_dictionary(response.read_antenna(dev))
 
-response.print_dictionary(response.read_antenna(dev))
+discover_reader()
+initialization()
+antenna_configuration()
+antenna_configuration_2()
+run_inventory()
+
 
 while 1:
-    response.print_dictionary(response.read_antenna(dev))
-
-
-
-
-
-
+    if response.read_antenna(dev) != None:
+        pass
+    else:
+        if usb.core.find(idVendor=0x24e9, idProduct=0x0861):
+            print 'ok'
+        else:
+            time.sleep(5)
+            print "LED OFF"
+            discover_reader()
+            initialization()
+            antenna_configuration()
+            antenna_configuration_2()
+            run_inventory()
 
 
 # # release the device
